f60a2c9f496c7bb9237b690869b9e66848c6ad8a
matiinrz <matinrezaii777@gmail.com>
2025-01-03 19:24:53 +0330

    Fix ChatLogs Table

--- database/migrations/2025_01_03_142345_create_chat_logs_table.php
+++ database/migrations/2025_01_03_142345_create_chat_logs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('input_messages');
             $table->text('response')->nullable();
-            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
