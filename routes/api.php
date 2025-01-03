3ad6d76660efceaef3e9a683f0fe2198135fe2f4
matiinrz <matinrezaii777@gmail.com>
2025-01-03 19:03:48 +0330

    Change api.php

--- routes/api.php
+++ routes/api.php
@@ -8,40 +8,33 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
-Route::middleware([
-    EnsureFrontendRequestsAreStateful::class,
-    'throttle:api',
-])->group(function () {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+    Route::get('/user', 'user')->middleware('auth:sanctum');
+});
+Route::get('/documentation', function () {
+    return view('swagger.index');
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/users/{id}', 'show');
+        Route::post('/users', 'store');
+        Route::put('/users/{id}', 'update');
+        Route::delete('/users/{id}', 'destroy');
 
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/register', 'register');
-        Route::post('/login', 'login');
-        Route::post('/logout', 'logout')->middleware('auth:sanctum');
-        Route::get('/user', 'user')->middleware('auth:sanctum');
-    });
-    Route::get('/documentation', function () {
-        return view('swagger.index');
-    });
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::controller(UserController::class)->group(function () {
-            Route::get('/users', 'index');
-            Route::get('/users/{id}', 'show');
-            Route::post('/users', 'store');
-            Route::put('/users/{id}', 'update');
-            Route::delete('/users/{id}', 'destroy');
+        // Chat API
+        Route::post('/chat', [ChatController::class, 'handleChat']);
 
-            // Chat API
-            Route::post('/chat', [ChatController::class, 'handleChat']);
-
-            // Log Management (Restricted to specific roles)
-            Route::middleware('can:viewAny,App\Models\ChatLog')->group(function () {
-                Route::get('/chat-logs', [ChatLogController::class, 'index']);
-                Route::delete('/chat-logs/{id}', [ChatLogController::class, 'destroy']);
-            });
+        // Log Management (Restricted to specific roles)
+        Route::middleware('can:viewAny,App\Models\ChatLog')->group(function () {
+            Route::get('/chat-logs', [ChatLogController::class, 'index']);
+            Route::delete('/chat-logs/{id}', [ChatLogController::class, 'destroy']);
         });
     });
-
 });
